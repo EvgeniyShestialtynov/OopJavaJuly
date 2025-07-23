997b15579ecbde72e45d7308dec34f263b6d7dcb
EvgeniyShestialtynov <Evgeniy.Shestialtynov@gmailcom>
2025-07-23 13:05:53 +0800

    Задача Range. Внесены исправления по письму от 04.08.24, внесены исправления в функцию getDifference.

--- Range/src/ru/academits/shestialtynov/range/Range.java
+++ Range/src/ru/academits/shestialtynov/range/Range.java
@@ -25,6 +25,11 @@ public class Range {
         this.to = to;
     }
 
+    @Override
+    public String toString() {
+        return "[" + from + " - " + to + "]";
+    }
+
     public double getLength() {
         return to - from;
     }
@@ -33,46 +38,55 @@ public class Range {
         return from <= number && number <= to;
     }
 
-    public Range getRangesCrossing(Range range) {
-        if ((from <= range.from && range.from < to) || (from < range.to && range.to <= to)) {
-            double rangesCrossingFrom = Math.max(from, range.from);
-            double rangesCrossingTo = Math.min(to, range.to);
-            return new Range(rangesCrossingFrom, rangesCrossingTo);
+    public Range getIntersection(Range range) {
+        if (isInside(range.from) || range.isInside(from)) {
+            double rangesIntersectionFrom = Math.max(from, range.from);
+            double rangesIntersectionTo = Math.min(to, range.to);
+
+            if (rangesIntersectionFrom == rangesIntersectionTo) {
+                return null;
+            }
+
+            return new Range(rangesIntersectionFrom, rangesIntersectionTo);
         }
 
         return null;
     }
 
-    public Range[] getRangesCombining(Range range) {
-        double minFrom = Math.min(from, range.from);
-        double maxFrom = Math.max(from, range.from);
-        double minTo = Math.min(to, range.to);
-        double maxTo = Math.max(to, range.to);
+    public Range[] getUnion(Range range) {
+        if (isInside(range.from) || range.isInside(from)) {
+            double minFrom = Math.min(from, range.from);
+            double maxTo = Math.max(to, range.to);
 
-        if (maxFrom == minTo) {
             return new Range[]{new Range(minFrom, maxTo)};
         }
 
-        return new Range[]{new Range(minFrom, maxFrom), new Range(minTo, maxTo)};
+        return new Range[]{this, range};
     }
 
-    public Range[] getRangesDifference(Range range) {
-        if ((range.to <= from) || (to <= range.from)) {
-            return new Range[]{new Range(from, to)};
+    public Range[] getDifference(Range range) {
+
+        if (range.from == from && range.to == to) {
+            return new Range[0];
         }
 
-        if ((range.from <= from) && (from < range.to) && (range.to < to)) {
-            return new Range[]{new Range(range.to, to)};
-        }
+        if (isInside(range.from) && range.from != to || range.isInside(from) && from != range.to) {
+            double minFrom = Math.min(from, range.from);
+            double maxFrom = Math.max(from, range.from);
+            double minTo = Math.min(to, range.to);
+            double maxTo = Math.max(to, range.to);
 
-        if (from < range.from) {
-            if (range.to < to) {
-                return new Range[]{new Range(from, range.from), new Range(range.to, to)};
+            if (minFrom == maxFrom) {
+                return new Range[]{new Range(minTo, maxTo)};
             }
 
-            return new Range[]{new Range(from, range.from)};
+            if (minTo == maxTo) {
+                return new Range[]{new Range(minFrom, maxFrom)};
+            }
+
+            return new Range[]{new Range(minFrom, maxFrom), new Range(minTo, maxTo)};
         }
 
-        return new Range[0];
+        return new Range[]{this, range};
     }
 }
