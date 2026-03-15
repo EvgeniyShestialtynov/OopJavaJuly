b991493bd9dae19ef8d5f9485ae6f94ae1b9cb4f
EvgeniyShestialtynov <Evgeniy.Shestialtynov@gmailcom>
2026-03-15 17:42:00 +0800

    Задача HashTable. Первая выгрузка задания на проверку.

--- HashTable/src/ru/academits/shestialtynov/hashtable/HashTable.java
+++ HashTable/src/ru/academits/shestialtynov/hashtable/HashTable.java
@@ -1,27 +1,102 @@
 package ru.academits.shestialtynov.hashtable;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
-import java.util.List;
 
 public class HashTable<E> implements Collection<E> {
-    private List<E>[] table;
+    private LinkedList<E>[] table;
     private int size;
+    private int modCount;
 
     public HashTable() {
         //noinspection unchecked
-        table = (List<E>[]) new List[100];
+        table = (LinkedList<E>[]) new LinkedList[100];
     }
 
     public HashTable(int capacity) {
         //noinspection unchecked
-        table = (List<E>[]) new List[capacity];
+        table = (LinkedList<E>[]) new LinkedList[capacity];
     }
 
     private int index(Object object) {
         return Math.abs(object.hashCode() % table.length);
+    }
+
+    @Override
+    public boolean add(E element) {
+        int index = index(element);
+
+        if (table[index] == null) {
+            table[index] = new LinkedList<>();
+        } else if (table[index].contains(element)) {
+            return false;
+        }
+
+        table[index].add(element);
+        size++;
+        modCount++;
+
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> collection) {
+        for (E element : collection) {
+            add(element);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object object) {
+        int index = index(object);
+
+        //noinspection unchecked
+        E element = (E) object;
+
+        if (table[index].remove(element)) {
+            size--;
+            modCount++;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        boolean removed = false;
+
+        for (Object element : collection) {
+            if (remove(element)) {
+                removed = true;
+            }
+        }
+
+        return removed;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        boolean removed = false;
+
+        for (E element : this) {
+            if (!collection.contains(element)) {
+                remove(element);
+                removed = true;
+            }
+        }
+
+        return removed;
+    }
+
+    @Override
+    public void clear() {
+        Arrays.fill(table, null);
+        size = 0;
+        modCount++;
     }
 
     @Override
@@ -46,104 +121,114 @@ public class HashTable<E> implements Collection<E> {
     }
 
     @Override
+    public boolean containsAll(Collection<?> collection) {
+        for (Object element : collection) {
+            if (!contains(element)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(table);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+
+        if (object == null || object.getClass() != getClass()) {
+            return false;
+        }
+
+        HashTable<?> hashTable = (HashTable<?>) object;
+
+        if (hashTable.size != size) {
+            return false;
+        }
+
+        for (int i = 0; i < table.length; i++) {
+            if (!Objects.equals(table[i], hashTable.table[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 37;
+        int hash = 1;
+
+        for (LinkedList<E> element : table) {
+            hash = prime * hash + (element == null ? 0 : element.hashCode());
+        }
+
+        return hash;
+    }
+
+    private class HashTableIterator implements Iterator<E> {
+        private int currentIndex = -1;
+        private final int initialModCount = modCount;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex + 1 < table.length;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("Хэш-таблица закончилась");
+            }
+
+            if (initialModCount != modCount) {
+                throw new ConcurrentModificationException("Хэш-таблица была изменена");
+            }
+            currentIndex++;
+
+            //noinspection unchecked
+            return (E) table[currentIndex];
+        }
+    }
+
+    @Override
     public Iterator<E> iterator() {
-        return null;
+        return new HashTableIterator();
     }
 
     @Override
     public Object[] toArray() {
-        Object[] array = new Object[size];
+        Object[][] array = new Object[table.length][];
         int i = 0;
-        for (E element : this) {
-            array[i++] = element;
+        for (LinkedList<E> element : table) {
+            array[i] = new LinkedList[]{element};
+            i++;
         }
+
         return array;
     }
 
     @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(E element) {
-        int index = index(element);
-
-        if (table[index].contains(element)) {
-            return false;
+    public <T> T[] toArray(T[] array) {
+        if (array.length < table.length) {
+            //noinspection unchecked
+            return (T[]) Arrays.copyOf(table, table.length, array.getClass());
         }
 
-        table[index].addFirst(element);
-        size++;
+        //noinspection SuspiciousSystemArraycopy
+        System.arraycopy(table, 0, array, 0, size);
 
-        return true;
-    }
-
-    @Override
-    public boolean remove(Object object) {
-        int index = index(object);
-
-        //noinspection unchecked
-        E element = (E) object;
-        if (table[index].remove(element)) {
-            size--;
-
-            return true;
+        if (table.length < array.length) {
+            array[table.length] = null;
         }
 
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> collection) {
-            for (Object element : collection) {
-                if (!contains(element)) {
-                    return false;
-            }
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> collection) {
-        for (E element : collection) {
-            add(element);
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> collection) {
-        boolean removed = false;
-
-        for (Object element : collection) {
-            if (remove(element)) {
-                removed = true;
-            };
-        }
-
-        return removed;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> collection) {
-        boolean removed = false;
-
-        for (E element : this) {
-            if (!collection.contains(element)) {
-                remove(element);
-                removed = true;
-            }
-        }
-
-        return removed;
-    }
-
-    @Override
-    public void clear() {
-        Arrays.fill(table, null);
-        size = 0;
+        return array;
     }
 }
--- HashTable/src/ru/academits/shestialtynov/main/Main.java
+++ HashTable/src/ru/academits/shestialtynov/main/Main.java
@@ -4,6 +4,46 @@ import ru.academits.shestialtynov.hashtable.HashTable;
 
 public class Main {
     public static void main(String[] args) {
-        HashTable<Object> store= new HashTable<>(200);
+        HashTable<String> hashTable = new HashTable<>();
+        System.out.println("Создана Хэш-таблица");
+
+        String name1 = "Семен";
+        String name2 = "Николай";
+        String name3 = "Игорь";
+        String name4 = "Ольга";
+        String name5 = "Василий";
+        String name6 = "Виктор";
+        String name7 = "Пересвет";
+        String name8 = "Василий";
+        String name9 = "Александр";
+
+        hashTable.add(name1);
+        hashTable.add(name2);
+        hashTable.add(name3);
+        hashTable.add(name4);
+        hashTable.add(name5);
+        hashTable.add(name6);
+        hashTable.add(name7);
+        hashTable.add(name8);
+        hashTable.add(name9);
+
+        System.out.println("Хэш-таблица заполнена, содержит " + hashTable.size() + " элементов");
+        System.out.println(hashTable);
+
+        if (hashTable.contains(name1)) {
+            System.out.println("Хэш-таблица содержит элемент \"" + name1 + "\"");
+        } else {
+            System.out.println("Хэш-таблица не содержит элемент \"" + name1 + "\"");
+        }
+
+        if (hashTable.remove(name2)) {
+            System.out.println("Из хэш-таблицы удален \"" + name2 + "\"");
+        } else {
+            System.out.println("Хэш-таблица не содержит элемент \"" + name2 + "\"");
+        }
+
+        hashTable.clear();
+        System.out.println("Хэш-таблица очищена");
+        System.out.println(hashTable);
+        }
     }
-}
